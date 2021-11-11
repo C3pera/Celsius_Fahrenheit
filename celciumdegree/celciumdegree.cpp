8a4d9f9848a6d9a87bf5545c004d04c8b3064131
Good <Good@GOOD->
2021-11-11 20:50:37 +0300

    Добавьте файлы проекта.

---
+++ celciumdegree/celciumdegree.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double convert(double);
+
+int main()
+
+{
+    double cel, fahr;
+    cout << "Введите значение Цельсия: ";
+    cin >> cel;
+
+    fahr = convert(cel);
+    cout << cel << "° Цельсия " << fahr << "° по Фаренгейту" << endl;
+}
+
+double convert(double cel)
+{
+    return 1.8 * cel + 32;
+}
